30411802c94847a74f8afd3bd3aad0ab3d26bcdc
puzhelev <puzhelev@gmail.com>
2023-06-10 17:50:13 +0300

    Add project files.

---
+++ Model/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DoJudo.Model.Database;
+using DoJudo.UI.Windows;
+
+namespace DoJudo.Model
+{
+    internal class User
+    {
+    }
+}
---
+++ View/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DoJudo.UI.Windows
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBoxPassword_Checked(object sender, RoutedEventArgs e)
+        {
+            TextBoxPassword.Text = PasswordBox.Password;
+        }
+
+        private void CheckBoxPassword_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Password = TextBoxPassword.Text;
+        }
+    }
+}
