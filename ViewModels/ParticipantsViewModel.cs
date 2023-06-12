03daac63169ece213802517a0eab3b86d296d94c
puzhelev <puzhelev@gmail.com>
2023-06-12 15:41:43 +0300

    made View page and viewModel

--- ViewModels/ParticipantsViewModel.cs
+++ ViewModels/ParticipantsViewModel.cs
@@ -1,14 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DoJudo.Models.Database;
+using DoJudo.Models.Repositories;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DoJudo.ViewModels
 {
     internal class ParticipantsViewModel : INotifyPropertyChanged
     {
+        private ObservableCollection<Participant> _participants;
+        private readonly string _nameCollectionParticipants = "Participants";
+        public ObservableCollection<Participant> Participants
+        {
+            get { return _participants; }
+            set
+            {
+                _participants = value;
+                OnPropertyChanged(_nameCollectionParticipants);
+            }
+        }
+        public ParticipantsViewModel()
+        {
+            var participantsList = DoJudoRepository.GetInstance().Participants.ToList();
+            Participants = new ObservableCollection<Participant>(participantsList);
+        }
         public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
