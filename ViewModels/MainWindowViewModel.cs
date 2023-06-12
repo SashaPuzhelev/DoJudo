65d342cb99ad56bf0c397ff1ddbfe904db356b04
puzhelev <puzhelev@gmail.com>
2023-06-12 13:56:24 +0300

    made navigate from MainWindow to some pages

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,30 +1,41 @@
 ﻿using DoJudo.Models;
-using DoJudo.Models.Repositories;
-using DoJudo.Models.Database;
+using GalaSoft.MvvmLight.Command;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DoJudo.ViewModels
 {
     internal class MainWindowViewModel : INotifyPropertyChanged
     {
-
+        private readonly string _pathToPages = "DoJudo.Views.Pages.";
+        private Page _currentPage;
+        public Page CurrentPage
+        {
+            get { return _currentPage; }
+            private set
+            {
+                _currentPage = value;
+                OnPropertyChanged(nameof(CurrentPage));
+            }
+        }
         public string FullNameWithRoleName
         {
             get { return CurrentUser.FullNameWithRoleName; }
         }
+        public ICommand NavigateCommand { get; set; }
         public MainWindowViewModel()
         {
-
+            NavigateCommand = new RelayCommand<string>((pageName) =>
+            {
+                CurrentPage = (Page)Activator.CreateInstance(Type.GetType(_pathToPages + pageName));
+            });
         }
         public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged (string propertyName)
+        protected void OnPropertyChanged(string propertyName)
         {
-            PropertyChanged?.Invoke (this, new PropertyChangedEventArgs (propertyName));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
---
+++ ViewModels/ParticipantsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoJudo.ViewModels
+{
+    internal class ParticipantsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
