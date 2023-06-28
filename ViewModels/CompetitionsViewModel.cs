dc41c8b5e352a515b023ce00ed7220e4d65570ef
puzhelev <puzhelev@gmail.com>
2023-06-29 02:11:08 +0300

    maded navigate to CompetitionPage from MainWindow

--- ViewModels/CompetitionsViewModel.cs
+++ ViewModels/CompetitionsViewModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DoJudo.ViewModels
 {
-    internal class CompetitionsViewModel
+    internal class CompetitionsViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
